f57dcb27ff31d9d52d3ff03e3a7fff8a6b1d8322
rh514 <rh514@student.le.ac.uk>
2026-02-07 09:42:07 +0000

    More renaming of User to Player

--- src/main/java/com/example/mahjong/service/UserDetails.java
+++ src/main/java/com/example/mahjong/service/UserDetails.java
@@ -1,23 +1,23 @@
 package com.example.mahjong.service;
 
-import com.example.mahjong.model.User;
-import com.example.mahjong.repository.UserRepository;
+import com.example.mahjong.model.Player;
+import com.example.mahjong.repository.PlayerRepository;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetails implements UserDetailsService {
-    private UserRepository userRepository; //User repository for interacting with the database
-    public UserDetails(UserRepository userRepository) { //Initialises the user repository
-        this.userRepository = userRepository;
+    private PlayerRepository playerRepository; //User repository for interacting with the database
+    public UserDetails(PlayerRepository playerRepository) { //Initialises the user repository
+        this.playerRepository = playerRepository;
     }
     @Override
     public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String username) throws UsernameNotFoundException { //Loads a user by username
-        User user = userRepository.findByUsername(username);
-        if (user == null) { //If the user is not found throw an error
+        Player player = playerRepository.findByUsername(username);
+        if (player == null) { //If the user is not found throw an error
             throw new UsernameNotFoundException("User not found");
         }
-        return org.springframework.security.core.userdetails.User.withUsername(user.getUsername()).password(user.getPassword()).roles(user.getRole()).build(); //Returns a User object
+        return org.springframework.security.core.userdetails.User.withUsername(player.getUsername()).password(player.getPassword()).roles(player.getRole()).build(); //Returns a User object
     }
 }
